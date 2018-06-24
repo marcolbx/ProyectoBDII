f04266427f74c0e2c7ef379b42800e72b9a41191
marcolbx <marcolbx@hotmail.com>
2018-06-24 13:46:10 -0400

    Creates y Drops

--- Drops.sql
+++ Drops.sql
@@ -3,22 +3,23 @@ drop table Transaccion;
 drop table Precio;
 drop table Mer_Mon;
 drop table Evento;
-drop table Moneda;
 drop table Mercado;
 drop table Orden_Market;
 drop table Orden_Limit;
 drop table Billetera;
+drop table Moneda;
 drop table Usuario;
 drop table Direccion;
-drop type Numero_de_Cuenta;
-drop type Descripcion;
-drop type Detalle;
-drop type Condicion;
-drop type Pais;
-drop type Telefono;
-drop type Pasaporte;
-drop type Fundadores;
 drop type Tarjeta;
 drop type Pago_Tarjeta;
+drop type Fundadores;
+drop type Pasaporte;
+drop type Telefono;
+drop type Pais;
+drop type Condicion;
+drop type Detalle;
+drop type Descripcion;
+drop type Numero_de_Cuenta;
+
 
 
