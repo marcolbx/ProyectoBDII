4029cb114388e7984b947c1f20155466e5821a47
vahevia <heviaveronica@gmail.com>
2018-06-30 20:20:16 -0400

    crear solicitudes listo

--- procedure_solicitudes_crear.sql
+++ procedure_solicitudes_crear.sql
@@ -17,20 +17,20 @@ CREATE or replace PROCEDURE P_Generacion_Solicitudes(num_solicitudes IN number,m
       mon_cantidad := dbms_random.value(0,10);
       select count (*) into cantidad_usuarios from Usuario where (usu_codigo = usuario);
       if cantidad_usuarios = 1 then
-       tipo_orden := dbms_random.value(1,2);
-       if(tipo_orden = 1) then
-          select pre_monto into cambio from precio where rownum=1 order by pre_fecha;
-          insert into Orden_Market(ord_mar_detalle,ord_mar_fecha_inicio,ord_mar_precio_actual,fk_usu_codigo,fk_mon_ofrecida_codigo, fk_mon_solicitada_codigo) values (Detalle(mon_cantidad,SYSDATE,cambio),SYSDATE,cambio,usuario,mon_solicitadas,mon_ofrecidas);
-          i:=i+1;
-       else
-          cambio:= dbms_random.value(precio_min,precio_max);
-          insert into Orden_Limit(ord_lim_detalle,ord_lim_fecha_inicio,ord_lim_condicion,fk_usu_codigo,fk_mon_ofrecida_codigo, fk_mon_solicitada_codigo) values (Detalle(mon_cantidad,SYSDATE,cambio),SYSDATE,Condicion(cambio,SYSDATE,'tipo'),usuario,mon_solicitadas,mon_ofrecidas);
-          i:=i+1;
-        end if;
+         tipo_orden := dbms_random.value(1,2);
+         if(tipo_orden = 1) then
+            select pre_monto into cambio from precio where rownum=1 order by pre_fecha;
+            insert into Orden_Market(ord_mar_detalle,ord_mar_fecha_inicio,ord_mar_precio_actual,fk_usu_codigo,fk_mon_ofrecida_codigo, fk_mon_solicitada_codigo) values (Detalle(mon_cantidad,SYSDATE,cambio),SYSDATE,cambio,usuario,mon_solicitadas,mon_ofrecidas);
+            i:=i+1;
+         else
+            cambio:= dbms_random.value(precio_min,precio_max);
+            insert into Orden_Limit(ord_lim_detalle,ord_lim_fecha_inicio,ord_lim_condicion,fk_usu_codigo,fk_mon_ofrecida_codigo, fk_mon_solicitada_codigo) values (Detalle(mon_cantidad,SYSDATE,cambio),SYSDATE,Condicion(cambio,SYSDATE,'tipo'),usuario,mon_solicitadas,mon_ofrecidas);
+            i:=i+1;
+          end if;
       end if;
       exit when i>=num_solicitudes;
     end loop;
 End;
 
-call P_Generacion_Solicitudes(10,1,2,0.1,2);
+call P_Generacion_Solicitudes(100,1,2,0.1,2);
  
